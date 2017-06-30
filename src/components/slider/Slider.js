d1e69dc82c38671a76f2d288de613583dc7bf62a
alpcanaydin <alpcan@alpcanaydin.com>
2017-06-30 21:24:11 +0300

    Slider 3 added.

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -3,6 +3,7 @@ import SlickSlider from 'react-slick';
 
 import Slide1 from './slides/Slide1';
 import Slide2 from './slides/Slide2';
+import Slide3 from './slides/Slide3';
 
 import './Slick.css';
 import './Slider.css';
@@ -14,6 +15,9 @@ const Slider = () => (
     </div>
     <div>
       <Slide2 />
+    </div>
+    <div>
+      <Slide3 />
     </div>
   </SlickSlider>
 );
