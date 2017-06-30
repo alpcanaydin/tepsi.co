51bda6c5d2ac0f038512f360f69edf609351b230
alpcanaydin <alpcan@alpcanaydin.com>
2017-07-01 00:43:34 +0300

    Shared components tests moved to correct folder.

--- __tests__/components/shared/Header.test.js
+++ __tests__/components/shared/Header.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Content from '../../src/components/Content';
+import Header from '../../../src/components/shared/Header';
 
 it('renders correctly', () => {
-  const wrapper = shallow(<Content><h1>Content</h1></Content>);
+  const wrapper = shallow(<Header />);
 
   expect(wrapper).toMatchSnapshot();
 });
